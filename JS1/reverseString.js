db5a83109087f34c1f8d656d6cc782e5a07fd71f
YashGoelSFDC <147296466+YashGoelSFDC@users.noreply.github.com>
2023-10-09 20:06:22 +0530

    Update reverseString.js

--- JS1/reverseString.js
+++ JS1/reverseString.js
@@ -1,12 +1,14 @@
 //Take a sentence as an input and reverse every word in that sentence. 
 //Example - This is a sunny day > shiT si a ynnus yad.
 
-
-const string = "This is a sunny day";
-
-function reverseString(str)
-{
-    return str.split("").reverse().join("");
+function reverseString(s){
+    let rev = "";
+    for(let i=(s.length)-1; i>=0; i--){
+        let ch = s[i];
+        rev += ch;
+    }
+    return rev;
 }
 
-console.log(reverseString(string));
+const str = "This is a sunny day";
+console.log(reverseString(str));
