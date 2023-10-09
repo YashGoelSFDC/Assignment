58f8114b62e9c095eeaa572de188a761a8e2d5f2
YashGoelSFDC <147296466+YashGoelSFDC@users.noreply.github.com>
2023-10-09 20:08:47 +0530

    Update sortArray.js

--- JS2/sortArray.js
+++ JS2/sortArray.js
@@ -1,12 +1,54 @@
 // Perform sorting of an array in descending order.
 
-let array = [45, 73, 9, 6, 18, 89, 2, -4, -65, 25];
+function merge(arr, left, middle, right) { 
+	let sorted = [arr.length];
+	
+    let l1 = left; 
+	let l2 = middle+1;
+    let x = 0; 
+	
+	while (l1 <= middle && l2 <= right) { 
+		if (arr[l1] > arr[l2]) { 
+			sorted[x] = arr[l1]; 
+			l1++; 
+		} else { 
+			sorted[x] = arr[l2]; 
+			l2++; 
+		} 
+		x++; 
+	} 
+	 
+	while (l1 <= middle) { 
+		sorted[x] = arr[l1]; 
+		l1++; 
+		x++; 
+	} 
+	while (l2 <= right) { 
+		sorted[x] = arr[l2]; 
+		l2++; 
+		x++; 
+	} 
+	
+	let j=left
+	for(let i=0; i<sorted.length; i++, j++){
+	    arr[j] = sorted[i];
+	}
+} 
+ 
+function mergeSort(arr, left, right) { 
+	if (left >= right) { 
+		return; 
+	} 
+	
+	let middle = left + parseInt((right - left) / 2); 
+	
+	mergeSort(arr, left, middle); 
+	mergeSort(arr, middle + 1, right); 
+	
+	merge(arr, left, middle, right); 
+} 
 
-function sortArray(arr)
-{
-    console.log("Unsorted Array:"+ arr);
-    arr.sort((a,b) => b-a);
-    console.log("Sorted Array:" + arr);
-}
-
-sortArray(array);
+const arr = [ 38, 27, 43, 10, 5, 41];
+console.log("Original array: " + arr); 
+mergeSort(arr, 0, arr.length - 1); 
+console.log("After sorting: " + arr);
